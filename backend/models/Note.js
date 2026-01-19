68861c2dd10553cd9591563b8e438f1d4d400eee
Nirasha <demelwgnd.22@uom.lk>
2026-01-19 11:20:27 +0530

    feat:create medels

---
+++ backend/models/Note.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+    title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  isPinned: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
+});
